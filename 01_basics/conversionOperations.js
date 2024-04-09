9482ad092788512f2b47d24e20210486ec0aad7e
Mangesh Ghadwaje <88280057+mangeshh09@users.noreply.github.com>
2024-04-09 14:51:46 +0000

    completed

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -16,7 +16,7 @@ console.log(convert)        //NaN (not a number)
 
 let login=1
 console.log(typeof login)
-console.log(login)
+console.log(login) 
 
 let convertlogin=Boolean(login)
 console.log(typeof convertlogin)
@@ -27,3 +27,22 @@ console.log(convertlogin)
 // 0=>false
 // "anystring" =>true
 // ""=>flase
+
+// =========================OPERATIONS================================
+console.log("OPERATIONS")
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+2+"3")
+console.log("1"+2+3)
+
+console.log("PREFIX AND POSTFIX OPERATOR")
+
+let x=10
+console.log(x)
+
+const z=x++
+console.log(x,z)
+
+const y=--x
+console.log(x,y)
+
