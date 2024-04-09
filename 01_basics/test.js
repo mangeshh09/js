5db8b67fad1e167fe9eb4a87da8ee84ede9c886a
Mangesh Ghadwaje <88280057+mangeshh09@users.noreply.github.com>
2024-04-09 06:57:47 +0000

    learn variables  and datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
 console.log("PRINT")
+
+function initialize(){    
+    var pie=3.14
+    if(true){
+        var pie=3.13
+        let neww=100
+        neww=101
+        console.table([pie,neww])
+    }
+    console.log(pie)
+    // console.log(neww)
+    // console.table([pie,neww])
+    
+}
+initialize()
