d3d784c13d16985c571658d53dfd3e1bf4319abb
Mangesh Ghadwaje <88280057+mangeshh09@users.noreply.github.com>
2024-04-09 07:19:59 +0000

    complete the datatype lecture

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,1 +1,17 @@
 "use strict"; //treat all code as a newer version
+
+// alert("print")  we are using node.js not browser
+
+//primitive datatypes
+
+//number 
+//bigint
+// String =>""  or ''
+// Boolean => true,false
+// null => standalone value()
+console.log(typeof null)        //object
+// undefined => undefined
+// Symbol =>unique
+
+//object
+
